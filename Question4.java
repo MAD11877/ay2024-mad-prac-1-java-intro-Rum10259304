4ca2090bc214f2f1613e845710f50a80e4fb2f15
Rum10259304 <151347327+Rum10259304@users.noreply.github.com>
2024-04-19 15:10:26 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -18,6 +18,16 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int n = in.nextInt();
+    for (int i = 0; i < n; i++)
+      {
+        for (int j = 0; j < n - i; j++)
+          {
+            System.out.print("*");
+          }
+        System.out.println();  
+      }
+      in.close();
+      
   }
 }
